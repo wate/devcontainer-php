58bbc397b6e94011c097e44baa6c0d6cf7de110b
wate <pc.poisoning@gmail.com>
2025-01-26 18:40:23 +0900

    update

---
+++ .devcontainer/provision/post_start.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [ -f composer.json ] && [ ! -e vendor ]; then
+  composer install --no-interaction
+fi
+
+if [ -f package.json ] && [ ! -e node_modules ]; then
+  ## @see https://github.com/antfu/ni
+  ni
+fi
+
+if type "direnv" >/dev/null 2>&1 && [ -f .envrc ]; then
+  direnv allow
+fi
+
+if type "ansible" >/dev/null 2>&1 && [ -f "$(dirname $0)/post_start.yml" ]; then
+  ansible-playbook  -i 127.0.0.1, -c local --diff "$(dirname $0)/post_start.yml"
+fi
+
+if [ -z "${__GIT_PROMPT_SHOW_CHANGED_FILES_COUNT}" ]; then
+  source "${HOME}/.bashrc"
+fi
+
+if [ -f .pre-commit-config.yaml ] && [ -e ~/.local/pipx/venvs/pre-commit ] && [ ! -f .git/hooks/pre-commit ]; then
+  pre-commit install
+fi
+
+apache2ctl start
