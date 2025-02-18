d532a46495dc0ac76624ca2c12041092c2f1b8f3
wate <pc.poisoning@gmail.com>
2025-02-18 22:07:26 +0900

    git-bash-promptの設定を調整

--- .devcontainer/provision/post_create.sh
+++ .devcontainer/provision/post_create.sh
@@ -3,19 +3,6 @@ set -eo pipefail
 
 echo "$(whoami):$(whoami)" | sudo chpasswd
 
-if [ ! -e ~/.bash-git-prompt ]; then
-  git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-  cat << EOT >>~/.bashrc
-# BEGIN bash-git-prompt setting ANSIBLE MANAGED BLOCK
-if [ -f "\$HOME/.bash-git-prompt/gitprompt.sh" ]; then
-    GIT_PROMPT_ONLY_IN_REPO=1
-    source \$HOME/.bash-git-prompt/gitprompt.sh
-fi
-# END bash-git-prompt setting ANSIBLE MANAGED BLOCK
-EOT
-
-fi
-
 sudo chmod a+x "$(pwd)"
 sudo rm -rf /var/www/html
 DOC_ROOT=$(pwd)
